077a73afcc815969ce11c76a384bef1400c07073
Daniel Harris <danielharris@Daniels-MBP.lan>
2021-12-06 11:37:30 +0100

    day 6

--- src/day06/day06.py
+++ src/day06/day06.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
         inp = list(map(int, f.readline().split(',')))
 
     @functools.lru_cache(maxsize=None)
-    def fish_for_days_left(days, timer):
-        return 1 + fish_for_days_left(days - timer - 1, 6) + fish_for_days_left(days - timer - 1, 8) if days > timer else 0
+    def simulate_production(days, timer):
+        return 1 + simulate_production(days - timer - 1, 6) + simulate_production(days - timer - 1, 8) if days > timer else 0
 
-    ans1 = len(inp) + sum([fish_for_days_left(80, num) for num in inp])
-    ans2 = len(inp) + sum([fish_for_days_left(256, num) for num in inp])
+    ans1 = len(inp) + sum([simulate_production(80, num) for num in inp])
+    ans2 = len(inp) + sum([simulate_production(256, num) for num in inp])
     print(f'answer to puzzle 1 is {ans1}')
     print(f'answer to puzzle 2 is {ans2}')

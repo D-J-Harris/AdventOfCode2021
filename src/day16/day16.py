2a22a8300bb01acadf94991ff0f7de8083f2fd57
Daniel Harris <danielharris@daniels-mbp.mynet>
2021-12-16 13:02:06 +0000

    day 16

--- src/day16/day16.py
+++ src/day16/day16.py
@@ -1,3 +1,4 @@
+import math
 
 if __name__ == "__main__":
 
@@ -38,43 +39,58 @@ if __name__ == "__main__":
                 f'version: {self.version}\n' +
                 f'is literal: {self.is_literal}\n' +
                 f'children: {self.children}\n' +
+                f'size: {self.size}\n' +
                 f'value: {self.value}')
 
 
     def parse(binary, parent):
-
         ptr = 0
         version = int(binary[ptr: (ptr := ptr + 3)], 2)
-        type = int(binary[ptr: (ptr := ptr + 3)], 2)
-        packet = Packet(version, type, parent)
+        packet_type = int(binary[ptr: (ptr := ptr + 3)], 2)
+        packet = Packet(version, packet_type, parent)
 
         if packet.is_literal:
             num = ''
             while int(binary[ptr: (ptr := ptr + 1)], 2):
                 num += binary[ptr: (ptr := ptr + 4)]
             num += binary[ptr: (ptr := ptr + 4)]
-            ptr += (ptr - 6) % 5
             packet.value = int(num, 2)
             packet.size = ptr
             return packet, binary[ptr:]
         else:
             length_type = int(binary[ptr: (ptr := ptr + 1)], 2)
             length_num_subpackets = int(binary[ptr: (ptr := ptr + (11 if length_type else 15))], 2)
-            binary = binary[ptr:]
+            child_binary = binary[ptr:]
             while length_num_subpackets > 0:
-                child_packet, binary = parse(binary, packet)
-                length_num_subpackets -= 1 if length_type else child_packet.size
+                child_packet, child_binary = parse(child_binary, packet)
                 packet.children.append(child_packet)
-            children_size = sum([x.size for x in packet.children])
-            packet_size = children_size + (4 - (children_size % 4))
-            while binary[packet_size: packet_size + 4] == '0000':
-                packet_size += 4
-            packet.size = packet_size
-            if parent:
-                parent.children.append(packet)
-            return packet, binary[packet.size:]
+                length_num_subpackets -= 1 if length_type else child_packet.size
+            ptr += sum([x.size for x in packet.children])
+            packet.size = ptr
+            match packet.packet_type:
+                case 0:
+                    packet.value = sum([x.value for x in packet.children])
+                case 1:
+                    packet.value = math.prod([x.value for x in packet.children])
+                case 2:
+                    packet.value = min([x.value for x in packet.children])
+                case 3:
+                    packet.value = max([x.value for x in packet.children])
+                case 5:
+                    packet.value = packet.children[0].value > packet.children[1].value
+                case 6:
+                    packet.value = packet.children[0].value < packet.children[1].value
+                case 7:
+                    packet.value = packet.children[0].value == packet.children[1].value
+            return packet, binary[ptr:]
 
 
-    p, _ = parse(puzzle_binary, None)
-    print(p)
-    [print(x) for x in p.children]
+    def version_sum(root_packet):
+        ans = root_packet.version
+        for child in root_packet.children:
+            ans += version_sum(child)
+        return ans
+
+    outer_packet, _ = parse(puzzle_binary, None)
+    print(f'answer to puzzle 1 is {version_sum(outer_packet)}')
+    print(f'answer to puzzle 2 is {outer_packet.value}')

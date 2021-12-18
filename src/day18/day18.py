ab782a8954059ff8965227dce41f42bef4c54a23
Daniel Harris <danielharris@daniels-mbp.mynet>
2021-12-18 09:04:51 +0000

    day 19

--- src/day18/day18.py
+++ src/day18/day18.py
@@ -6,10 +6,17 @@ if __name__ == "__main__":
 
     class SnailFish:
         def __init__(self, inp):
-            self.pair = literal_eval(inp) if isinstance(inp, str) else inp
-            self.left = self.pair[0]
-            self.right = self.pair[1]
-            self.magnitude = SnailFish.calculate_magnitude(self.left, self.right)
+            self._pair = literal_eval(inp) if isinstance(inp, str) else inp
+            self.left = self._pair[0]
+            self.right = self._pair[1]
+
+        def set_pair(self, inp):
+            self._pair = literal_eval(inp) if isinstance(inp, str) else inp
+            self.left = self._pair[0]
+            self.right = self._pair[1]
+
+        def get_pair(self):
+            return self._pair
 
         @staticmethod
         def calculate_magnitude(left, right):
@@ -22,87 +29,80 @@ if __name__ == "__main__":
         @classmethod
         def add_together(cls, left, right):
             assert(isinstance(left, SnailFish) and isinstance(right, SnailFish))
-            snailfish = SnailFish([left.pair, right.pair])
-            print(snailfish.pair)
+            snailfish = SnailFish([left.get_pair(), right.get_pair()])
             reduced = False
             while not reduced:
                 if cls.__explode(snailfish):
-                    print(snailfish.pair)
                     continue
-                reduced = cls.__split(snailfish)
-            print('haha ', snailfish.pair)
+                reduced = not cls.__split(snailfish)
+            return snailfish
 
-            
-        
+
         def __explode(snailfish):
-            string = str(snailfish.pair)
-            str_iterator = enumerate(string)
+            string = str(snailfish.get_pair())
 
-            open_brackets, open_loc, close_loc = 0, None, None
+            open_brackets, open_loc, comma_found, close_loc = 0, None, False, None
             left_element, right_element = '', ''
-            while (nxt := next(str_iterator)) is not None:
-                open_brackets += 1 if nxt[1] == '[' else 0
-                open_brackets -= 1 if nxt[1] == ']' else 0
-                if open_brackets == 5:
-                    open_loc = nxt[0]
-                    while (nxt := next(str_iterator))[1] != ',':
-                        left_element += nxt[1]
-                    while (nxt := next(str_iterator))[1] != ']':
-                        right_element += nxt[1]
-                    close_loc = nxt[0]
-                    left_element, right_element = int(left_element), int(right_element)
-                    break
+            for idx, char in enumerate(string):
+                if open_brackets < 5:
+                    open_brackets += 1 if char == '[' else 0
+                    open_brackets -= 1 if char == ']' else 0
+                    if open_brackets == 5:
+                        open_loc = idx
+                else:
+                    if not comma_found:
+                        if char == ',':
+                            comma_found = True
+                        else:
+                            left_element += char
+                    elif not close_loc:
+                        if char == ']':
+                            close_loc = idx
+                            left_element, right_element = int(left_element), int(right_element)
+                            break
+                        else:
+                            right_element += char
 
-            if matched := re.search(r'^\D*(\d+)', string[close_loc:]):
-                string = string[:close_loc] + re.sub(r'^(\D*)\d+', fr'\1 {right_element + int(matched.group(1))}', string[close_loc:])
-            string = string[:open_loc] + '0' + string[close_loc + 1:]
-            if matched := re.search(r'(\d+)\D*$', string[:open_loc]):
-                string = re.sub(r'\d+(\D*)$', fr'{left_element + int(matched.group(1))}\1', string[:open_loc]) + string[open_loc:]
-            snailfish.pair = string
-            return True if open_brackets else False
+            if open_brackets == 5:
+                if matched := re.search(r'^\D*(\d+)', string[close_loc:]):
+                    string = string[:close_loc] + re.sub(r'^(\D*)\d+', fr'\1 {right_element + int(matched.group(1))}', string[close_loc:])
+                string = string[:open_loc] + '0' + string[close_loc + 1:]
+                if matched := re.search(r'(\d+)\D*$', string[:open_loc]):
+                    string = re.sub(r'\d+(\D*)$', fr'{left_element + int(matched.group(1))}\1', string[:open_loc]) + string[open_loc:]
+                snailfish.set_pair(string.replace(' ', ''))
+                return True
+            else:
+                return False        
         
+
         def __split(snailfish):
-            string = str(snailfish.pair)
-            if matched := re.search(r'\d{2,}(.*)', string):
+            string = str(snailfish.get_pair())
+            if matched := re.search(r'(\d{2,}).*', string):
                 num = int(matched.group(1))
-                string = re.sub(r'(\d{2,}).*', fr'{[floor(num / 2), ceil(num / 2)]}\1')
-
-            snailfish.pair = string
-            return True if matched else False
-
-
-
-                
-
-
-
+                string = re.sub(r'\d{2,}(.*)', fr'{[floor(num / 2), ceil(num / 2)]}\1', string)
+                snailfish.set_pair(string.replace(' ', ''))
+                return True
+            else:
+                return False
 
     with open('inputs/input-18.txt', 'r') as f:
         snailfish = []
         for line in f.read().splitlines():
             snailfish.append(SnailFish(line))
 
-    one_snail = SnailFish(r'[[[[4,3],4],4],[7,[[8,4],9]]]')
-    two_snail = SnailFish(r'[1,1]')
-    # one_snail = SnailFish(r'[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]')
-    # two_snail = SnailFish(r'[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]')
-    res = SnailFish.add_together(one_snail, two_snail)
-    print('[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]')
-
-    # string = '[[[[0,7],4],[7,[[8,4],9]]],[1,1]]'
-    # open = 16
-    # close = 20
-    # print(string)
-    # matched_num = re.search(r'^\D*(\d+)', string[close:]).group(1)
-    # print(matched_num)
-    # string = string[:close] + re.sub(r'^(\D*)\d+', fr'\1 {12 + int(matched_num)}', string[close:])
-    # print(string)
-    # string = string[:open] + '0' + string[close + 1:]
-    # print(string)
-    # matched_num = re.search(r'(\d+)\D*$', string[:open]).group(1)
-    # print(matched_num)
-    # string = re.sub(r'\d+(\D*)$', fr'{12 + int(matched_num)}\1', string[:open]) + string[open:]
-    # print(string)
-
-    ans1 = 0
+    res = snailfish[0]
+    for sn in snailfish[1:]:
+        res = res.add_together(res, sn)
+    ans1 = SnailFish.calculate_magnitude(res.left, res.right)
     print(f'answer to puzzle 1 is {ans1}')
+
+    ans2 = 0
+    for idx, sn1 in enumerate(snailfish):
+        for sn2 in snailfish[idx + 1:]:
+            res = SnailFish.add_together(sn1, sn2)
+            mag = SnailFish.calculate_magnitude(res.left, res.right)
+            ans2 = max(ans2, mag)
+            res = SnailFish.add_together(sn2, sn1)
+            mag = SnailFish.calculate_magnitude(res.left, res.right)
+            ans2 = max(ans2, mag)
+    print(f'answer to puzzle 2 is {ans2}')
